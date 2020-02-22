aeb462116a0266154cbd5210a9371f6fb5b578ef
natyrix <natrix277@gmail.com>
2020-02-22 17:07:27 +0300

    modefied hotel's admin

--- Hotel/admin.py
+++ Hotel/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Hotel
+from .models import Hotel, HotelGallery
 # Register your models here.
 
 admin.site.register(Hotel)
+admin.site.register(HotelGallery)
